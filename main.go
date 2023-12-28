0777f42f51446775bcd5677a3547200c3c69c92f
Calle CJM Robertsson <calle@robcon.se>
2023-12-28 23:00:20 +0100

    Add customized help option.

--- main.go
+++ main.go
@@ -11,9 +11,13 @@ import (
 
 var pbFile = "planban.json" // default board file name
 
-var printFlag = false
+var (
+	helpFlag  = false
+	printFlag = false
+)
 
 func init() {
+	flag.BoolVar(&helpFlag, "h", false, "print help and exit")
 	flag.BoolVar(&printFlag, "p", false, "print board and exit")
 	flag.Parse()
 
@@ -21,12 +25,18 @@ func init() {
 	if file != "" {
 		pbFile = file
 	}
+
 }
 
 func main() {
 	pb, err := app.New(pbFile)
 	if err != nil {
 		fail(err)
+	}
+
+	if helpFlag {
+		help()
+		os.Exit(0)
 	}
 
 	if printFlag {
@@ -44,4 +54,23 @@ func main() {
 func fail(a ...any) {
 	fmt.Fprintf(os.Stderr, "Exit with error: %q\n", a...)
 	os.Exit(1)
+}
+func help() {
+	fmt.Printf(`Planban is a simple command line Kanban application.
+
+Usage:
+  planban [flags] [<filename>.json]
+
+  If the file doesn't exist it will be created. If no filename is given,
+  the default filename "planban.json" will be used.
+
+  The available commands can be shown by pressing enter key in planban.
+
+Options:
+`)
+	flag.PrintDefaults()
+
+	fmt.Printf(`
+Please visit github.com/callerobertsson/planban for more information
+`)
 }
